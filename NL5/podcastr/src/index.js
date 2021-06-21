d1763256882847c3c432ed8daead8c5818566b15
Lucas Kaminski <lucas.kssilveira@gmail.com>
2021-06-21 09:37:07 -0300

    nlw6

--- NL5/podcastr/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; //Importa o React
-import ReactDOM from 'react-dom'; // Integração entre React e WEB
-import App from './App'; //Outro arquivo app
-
-ReactDOM.render( //Renderizer em tela o app dentro do documento que tem id root
-  <App />, //função app da app.js
-  document.getElementById('root')
-);
-
