d901febf2fc55a508487099f6dba1bfe6f882c05
Lucas Kaminski <lucas.kssilveira@gmail.com>
2021-07-16 14:59:06 -0300

    restart on portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,36 +1,5 @@
-import About from "./components/About";
-import Frontpage from "./components/Frontpage";
-import Menu from "./components/Menu";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Blog from "./components/Blog";
-import Contact from "./components/Contact";
-
-import MainContextProvider from "./contexts/MainContextProvider";
-
-import TrackVisibility from 'react-on-screen';
-import { Box } from '@chakra-ui/react'
-import Leftbar from "./components/Leftbar";
-
 function App() {
-  return (
-    <MainContextProvider>
-      <Box className="App" bg="primaryDark" >
-        <Menu />
-        <Leftbar />
-        <TrackVisibility partialVisibility>
-          <Frontpage />
-        </TrackVisibility>
-        <Box ml="4vw" mt="10vh">
-          <About />
-          <Experience />
-          <Projects />
-          <Blog />
-          <Contact />
-        </Box>
-      </Box>
-    </MainContextProvider >
-  )
+  return <div className="App">aa</div>
 }
 
-export default App;
+export default App
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,31 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react'
-
-import { extendTheme } from "@chakra-ui/react"
-
-const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
-}
-
-const fonts = {
-  body: "Ubuntu",
-}
-
-const colors = {
-  primaryDark: "#030303",
-  primaryLight: "#F9F9F9"
-}
-
-const theme = extendTheme({ config, fonts, colors })
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
