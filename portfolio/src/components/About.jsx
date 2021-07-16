ebf78b2332c3e89552ec70ef33e17e521b7a379f
Lucas Kaminski <lucas.kssilveira@gmail.com>
2021-07-16 00:12:28 -0300

    little updates

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,10 +1,13 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import Default from "./Default";
 
 export default function About() {
   return (
-    <Flex id="about" h="100vh">
-      Sobre mim
-    </Flex>
+    <Default id="about" title="Sobre mim">
+      <Flex pt={10} direction="column" textAlign="justify" w="70%">
+        <Text>Olá! Tudo bem? Meu nome é Lucas Kaminski e eu amo criar</Text>
+      </Flex>
+    </Default>
   );
 }
